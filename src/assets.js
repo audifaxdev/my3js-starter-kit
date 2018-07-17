d5c4e1d84bb651c2b5f0e0f8b866bbd57fbc16b5
Kevin Barreau <kevin.barreau@youngshand.com>
2018-07-17 18:51:30 +1200

    remove unneeded asset

--- src/assets.js
+++ src/assets.js
@@ -1,7 +1,6 @@
 import {genCubeUrls} from "./utils/hdr";
 
 export default [
-  {type: 'GLTF', url: '/DamagedHelmet.gltf', id: 'helmet'},
   {type: 'GLTF', url: '/CompanionCube.gltf', id: 'scene'},
   {type: 'GLTF', url: '/test_01.gltf', id: 'myMask'},
   {type: 'HDRCubeMap', url: genCubeUrls( "./dist/textures/HollywoodBD/", ".hdr" ), id: 'hdrCube'}
